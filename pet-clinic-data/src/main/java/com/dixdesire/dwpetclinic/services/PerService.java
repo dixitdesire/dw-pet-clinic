1e829d0df4c17ec5b6c034681218d7f7a5986bce
dixdesire <dixitwadhwani@gmail.com>
2019-08-26 19:13:28 +0530

    Refactor service interface to common service interface.

--- pet-clinic-data/src/main/java/com/dixdesire/dwpetclinic/services/PerService.java
+++ pet-clinic-data/src/main/java/com/dixdesire/dwpetclinic/services/PerService.java
@@ -2,12 +2,6 @@ package com.dixdesire.dwpetclinic.services;
 
 import com.dixdesire.dwpetclinic.model.Pet;
 
-import java.util.Set;
+public interface PerService extends CrudService<Pet, Long>{
 
-public interface PerService {
-    Pet findById(Long id);
-
-    Pet save(Pet pet);
-
-    Set<Pet> findAll();
 }
