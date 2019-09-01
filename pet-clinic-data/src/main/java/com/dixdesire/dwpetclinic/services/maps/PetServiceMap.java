3381c6a0fdb4754902c215fcd1e3b9da4dbfa993
dixdesire <dixitwadhwani@gmail.com>
2019-09-02 01:00:38 +0530

    Update Map Impl to manage setting of ID Property

--- pet-clinic-data/src/main/java/com/dixdesire/dwpetclinic/services/maps/PetServiceMap.java
+++ pet-clinic-data/src/main/java/com/dixdesire/dwpetclinic/services/maps/PetServiceMap.java
@@ -25,7 +25,7 @@ public class PetServiceMap extends AbstractMapService<Pet, Long> implements Crud
 
     @Override
     public Pet save(Pet owner) {
-        return super.save(owner.getId(), owner);
+        return super.save(owner);
     }
 
     @Override
