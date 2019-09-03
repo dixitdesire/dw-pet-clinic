e6cbd55fce48dccc4795254fbae924105b0d3977
dixdesire <dixitwadhwani@gmail.com>
2019-09-03 20:12:40 +0530

    Added specialities map service impl

---
+++ pet-clinic-data/src/main/java/com/dixdesire/dwpetclinic/services/SpecialitiesService.java
@@ -0,0 +1,6 @@
+package com.dixdesire.dwpetclinic.services;
+
+import com.dixdesire.dwpetclinic.model.Speciality;
+
+public interface SpecialitiesService extends CrudService<Speciality, Long> {
+}
---
+++ pet-clinic-data/src/main/java/com/dixdesire/dwpetclinic/services/maps/SpecialitiesServiceMap.java
@@ -0,0 +1,35 @@
+package com.dixdesire.dwpetclinic.services.maps;
+
+import com.dixdesire.dwpetclinic.model.Speciality;
+import com.dixdesire.dwpetclinic.services.SpecialitiesService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class SpecialitiesServiceMap extends AbstractMapService<Speciality, Long> implements SpecialitiesService {
+    @Override
+    public Set<Speciality> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Speciality obj) {
+        super.delete(obj);
+    }
+
+    @Override
+    public Speciality save(Speciality obj) {
+        return super.save(obj);
+    }
+
+    @Override
+    public Speciality findById(Long id) {
+        return super.findById(id);
+    }
+}
