f9215d8ecfb0e6a3fceaebb2146015529b7efdb8
dixdesire <dixitwadhwani@gmail.com>
2019-09-04 14:09:31 +0530

    adding spring data JPA owner service

--- pet-clinic-data/src/main/java/com/dixdesire/dwpetclinic/repositories/OwnerRepository.java
+++ pet-clinic-data/src/main/java/com/dixdesire/dwpetclinic/repositories/OwnerRepository.java
@@ -4,4 +4,5 @@ import com.dixdesire.dwpetclinic.model.Owner;
 import org.springframework.data.repository.CrudRepository;
 
 public interface OwnerRepository extends CrudRepository<Owner, Long> {
+    Owner findByLastName(String lastName);
 }
